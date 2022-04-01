f9c88899a9657ac8f2fe473a7425792f3495c183
JoaquinJPB <jjpb2000@gmail.com>
2022-04-01 01:34:17 +0100

    Set Up + Template Sign In/Up

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import SignUp from './components/Signup';
+import SignIn from './components/Signin';
+
+function App() {
+    return (
+        <div>
+            <SignUp />
+            <SignIn />
+        </div>
+    )
+}
+
+export default App;
