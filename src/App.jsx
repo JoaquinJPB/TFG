b77fb676da91bc7e6b7794cfbc0f75ef7e422bde
JoaquinJPB <jjpb2000@gmail.com>
2022-06-24 20:30:20 +0100

    Implementing React Router Dom v6

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,16 @@ import ItemsGrid from './components/ItemsGrid';
 import items from "./movies.json"
 //import styles from './styles/App.module.css'
 
+import { BrowserRouter as Router, Routes, Route, Link, Navigate} from 'react-router-dom'
+
 const App = () => {
     return (
         <div>
-            <main>
-                <ItemsGrid title={"Prueba"} subtitle={"qweqweqwewqe"} items={items}/>
-            </main>
+            <Routes>
+                <Route path='/movies' element={<ItemsGrid title={"Recomendaciones de peliculas"} subtitle={"Prueba peliculas"} items={items} />} />
+                <Route path='/books' element={<ItemsGrid title={"Recomendaciones de libros"} subtitle={"Prueba libros"} items={items} />} />
+                <Route path='/videogames' element={<ItemsGrid title={"Recomendaciones de videojuegos"} subtitle={"Prueba de videojuegos"} items={items}/>} />
+            </Routes>
         </div>
     )
 }
