b8c0b9a5f26e7e770e43282097e245c40b1a7ce0
JoaquinJPB <jjpb2000@gmail.com>
2022-08-27 20:30:26 +0100

    Grid MUI

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -5,44 +5,52 @@ import imgBreathing from "../images/breathing_section.png"
 import imgMeditation from "../images/meditation_section.png"
 import imgRecommendations from "../images/recommendations_section.png"
 import imgAdvice from "../images/advice_section.png"
+import { Grid } from "@mui/material"
 
 const Home = () => {
   return (
-      <article className={styles.articleHome}>
-        <section className={styles.headerHome}>
-          <h1>Redux Toolkit</h1>
-          <h3>
-            The official, opinionated, batteries-included toolset for efficient
-            Redux development
-          </h3>
-          <button>
-            <span className={styles.button_top}>Button</span>
-          </button>
-        </section>
-        <div className={styles.homeCards}>
+    <article className={styles.articleHome}>
+      <section className={styles.headerHome}>
+        <h1>Bloodborne PC Remastered</h1>
+        <h3>
+          The official, opinionated, batteries-included toolset for efficient
+          Redux development
+        </h3>
+        <button>
+          <span className={styles.button_top}>Button</span>
+        </button>
+      </section>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6} lg={3}  display={"flex"} justifyContent={"center"}>
           <HomeSectionCard
             title={"Técnicas de respiración"}
             img={imgBreathing}
             route={"breathing"}
           />
+        </Grid>
+        <Grid item xs={12} sm={6} lg={3} display={"flex"} justifyContent={"center"}>
           <HomeSectionCard
             title={"Meditación"}
             img={imgMeditation}
             route={"meditation"}
           />
+        </Grid>
+        <Grid item xs={12} sm={6} lg={3} display={"flex"} justifyContent={"center"}>
           <HomeSectionCard
             title={"Recomendaciones"}
             img={imgRecommendations}
             route={"recommendations"}
           />
+        </Grid>
+        <Grid item xs={12} sm={6} lg={3} display={"flex"} justifyContent={"center"}>
           <HomeSectionCard
             title={"Consejos"}
             img={imgAdvice}
             route={"advice"}
           />
-        </div>
-        <section></section>
-      </article>
+        </Grid>
+      </Grid>
+    </article>
   )
 }
 
