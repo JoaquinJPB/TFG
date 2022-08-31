7af5715aaa65eefd21de9dcbe135014f9c58d262
JoaquinJPB <jjpb2000@gmail.com>
2022-08-31 21:31:06 +0100

    ItemsGrid CSS Updating

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -1,19 +1,23 @@
-import { Grid } from "@mui/material";
-import styles from "../styles/ItemCard.module.css";
+import { Grid } from "@mui/material"
+import styles from "../styles/ItemCard.module.css"
 
 const ItemCard = ({ item }) => {
   return (
-    <Grid item xs={12} md={3} className={styles.itemCard}>
-      <div>
-        <img src={item.img} alt={item.title} className={styles.itemImage} />
-      </div>
-      <div className={styles.itemCardInfo}>
-        <p className={styles.textTitle}>{item.title}</p>
-        <p className={styles.textSubtitle}>{item.author || item.director || item.developer}</p>
-        <button className={styles.cardButton}>Read More</button>
+    <Grid item xs={12} sm={6} md={4} lg={3}>
+      <div className={styles.itemCard}>
+        <div>
+          <img src={item.img} alt={item.title} className={styles.itemImage} />
+        </div>
+        <div className={styles.itemCardInfo}>
+          <p className={styles.textTitle}>{item.title}</p>
+          <p className={styles.textSubtitle}>
+            {item.author || item.director || item.developer}
+          </p>
+          <button className={styles.cardButton}>Read More</button>
+        </div>
       </div>
     </Grid>
-  );
-};
+  )
+}
 
-export default ItemCard;
+export default ItemCard
--- src/components/ItemsGrid.jsx
+++ src/components/ItemsGrid.jsx
@@ -6,18 +6,20 @@ import ItemCard from "./ItemCard"
 const ItemsGrid = ({ title, subtitle, items, background }) => {
   return (
     <article className={styles.gridArticle}>
-      <Grid container px={5}>
+      <Grid container px={5} spacing={4}>
         <Grid item xs={12} md={6}>
           <Grid container>
-            <Header title={title} subtitle={subtitle} />
-            <Grid container className={styles.itemsGrid}>
+            <Header title={title} subtitle={subtitle}/>
+            <Grid container className={styles.itemsGrid} spacing={2}>
               {items.data.map((item) => (
                 <ItemCard key={item.id} item={item} />
               ))}
             </Grid>
           </Grid>
         </Grid>
-        <Grid item xs={12} md={6}>
+        <Grid item xs={12} md={6} display={"flex"}
+        justifyContent={"center"}
+        alignItems={"center"}>
           <img
             src={background}
             alt="Background section"
