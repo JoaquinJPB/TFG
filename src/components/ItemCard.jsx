15efb9ae773e1716b462a369b22c3b202219a608
JoaquinJPB <jjpb2000@gmail.com>
2022-09-19 21:56:23 +0100

    Item now has a modal

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -1,7 +1,12 @@
-import { Grid } from "@mui/material"
+import { Grid, Box, Modal } from "@mui/material"
 import styles from "../styles/ItemCard.module.css"
+import { useState } from "react"
 
 const ItemCard = ({ item }) => {
+  const [open, setOpen] = useState(false)
+  const handleOpen = () => setOpen(true)
+  const handleClose = () => setOpen(false)
+
   return (
     <Grid item xs={12} sm={6} md={4} lg={3}>
       <div className={styles.itemCard}>
@@ -13,9 +18,20 @@ const ItemCard = ({ item }) => {
           <p className={styles.textSubtitle}>
             {item.author || item.director || item.developer}
           </p>
-          <button className={styles.cardButton}>Read More</button>
+          <button className={styles.cardButton} onClick={handleOpen}>Más info</button>
         </div>
       </div>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box className={styles.boxModal}>
+          <h2 className={styles.title}>{item.title}</h2>
+          <p className={styles.description}>{item.description}</p>
+        </Box>
+      </Modal>
     </Grid>
   )
 }
