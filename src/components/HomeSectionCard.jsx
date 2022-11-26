4ea127ec47bce37ef8e505b630f98fd181a50a1b
JoaquinJPB <jjpb2000@gmail.com>
2022-11-26 13:23:18 +0000

    HomeSectionCard refactor MUI

--- src/components/HomeSectionCard.jsx
+++ src/components/HomeSectionCard.jsx
@@ -1,14 +1,38 @@
+import { Box, Typography } from "@mui/material"
 import { useNavigate } from "react-router-dom"
-import styles from "../styles/HomeSectionCard.module.css"
 
 const HomeSectionCard = ({ title, img, route }) => {
-	const navigate = useNavigate()
-	return (
-		<div className={styles.cardBody} onClick={() => navigate(route)}>
-			<img src={img} alt={title} />
-			<h3>{title}</h3>
-		</div>
-	)
+  const navigate = useNavigate()
+
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      justifyContent="space-around"
+      alignItems="center"
+      textAlign="center"
+      mt={4}
+      sx={{
+        backgroundColor: "var(--primaryColor)",
+        borderRadius: "2rem",
+        boxShadow: "-3px 7px 5px -3px rgba(0, 0, 0, 0.75)",
+        width: "100%",
+        height: "100%",
+        border: "2px solid transparent",
+        "&:hover": {
+          opacity: "0.8",
+          border: "2px solid black",
+          cursor: "pointer",
+        },
+      }}
+      onClick={() => navigate(route)}
+    >
+      <Box component="img" src={img} alt={title} />
+      <Typography Typography variant="h5" component="h3" color="#fff">
+        {title}
+      </Typography>
+    </Box>
+  )
 }
 
 export default HomeSectionCard
